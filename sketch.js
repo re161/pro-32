4752f6eed86a22d5d7f17729357572206520a218
re161 <77871348+re161@users.noreply.github.com>
2022-09-13 17:51:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -52,7 +52,7 @@ async function getBackgroundImg(){
     var datetime = responseJson.datetime;
     console.log(datetime);
     // slice the datetime to extract hour
-    var hour = datetime.slice(11,13);
+    hour = datetime.slice(11,13);
     console.log(hour);
      if(hour>=2 && hour<18 ){
         bg = "sunrise.png";
